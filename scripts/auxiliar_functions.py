4827700c370ca6c0ef242c6ac83ce5516d6f71c2
andromedalactea <bry3639@gmail.com>
2024-09-27 10:47:37 -0500

    [FEAT] using chroma db

--- scripts/auxiliar_functions.py
+++ scripts/auxiliar_functions.py
@@ -1,3 +1,5 @@
+import os
+
 def sources_to_md(sources: list) -> str:
     """
     Convert a list of sources to a Markdown formatted string.
@@ -10,7 +12,7 @@ def sources_to_md(sources: list) -> str:
     """
 
     #  Generate the urls 
-    URLS = [f"https://chat.petromentor.ai/pdfs/{source.replace('/mnt/i/', '').replace(' ', '%20')}" for source in sources]
+    URLS = [f"https://www.petrowhiz.ai/pdfs/{source.replace('/mnt/apec-ai-feed/', '').replace(' ', '%20')}" for source in sources]
 
     # References
     references = [source.split("/")[-1] for source in sources]
@@ -19,3 +21,6 @@ def sources_to_md(sources: list) -> str:
     md_sources = "\n".join([f"- [{reference}]({url})" for reference, url in zip(references, URLS)])
 
     return md_sources
+
+def absolute_path(relative_path):
+    return os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), relative_path))
--- scripts/extract_context_from_vs.py
+++ scripts/extract_context_from_vs.py
@@ -1,39 +1,48 @@
 import os
 
+from scripts.auxiliar_functions import absolute_path
+
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 
 # Load the environment variables
 load_dotenv(override=True)
 
 def extract_context_from_vector_search(query: str = 'white', k: int = 4):
 
-    MONGO_URI=os.environ["URI_MONGODB_VECTOR_SEARCH"]
-    DB_NAME = "apec_db"
-    COLLECTION_NAME_EMBEDDING = "apec_vectorstores_official"
-    ATLAS_VECTOR_SEARCH_INDEX_NAME = "vector_index"
+    # MONGO_URI=os.environ["URI_MONGODB_VECTOR_SEARCH"]
+    # DB_NAME = "apec_db"
+    # COLLECTION_NAME_EMBEDDING = "apec_vectorstores_official"
+    # ATLAS_VECTOR_SEARCH_INDEX_NAME = "vector_index"
 
-    # Load the environment variables
-    load_dotenv(override=True)
-    # Configure the MongoDB client
-    client = MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME_EMBEDDING]
+    # # Configure the MongoDB client
+    # client = MongoClient(MONGO_URI)
+    # db = client[DB_NAME]
+    # collection = db[COLLECTION_NAME_EMBEDDING]
 
     # # Filter to extract only the documents in the filtered list
     # filter_spec = {"id": {"$in": ids_filtered_docs}}
 
-    # Create the connection to MongoDB and VectorSearch
-    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-        MONGO_URI,
-        DB_NAME + "." + COLLECTION_NAME_EMBEDDING,
-        embedding=OpenAIEmbeddings(disallowed_special=(), model='text-embedding-3-large'),
-        index_name = ATLAS_VECTOR_SEARCH_INDEX_NAME,
-    )
+    # # Create the connection to MongoDB and VectorSearch
+    # vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+    #     MONGO_URI,
+    #     DB_NAME + "." + COLLECTION_NAME_EMBEDDING,
+    #     embedding=OpenAIEmbeddings(disallowed_special=(), model='text-embedding-3-large'),
+    #     index_name = ATLAS_VECTOR_SEARCH_INDEX_NAME,
+    # )
 
+    # Data directory absolute path
+    data_directory = absolute_path("../data/APEC_ChromaDB")
+
+    vector_search= Chroma(
+                    collection_name="apec_vectorstores",
+                    embedding_function=OpenAIEmbeddings(disallowed_special=(), model='text-embedding-3-large'),
+                    persist_directory=data_directory,  
+                )
     # Perform the similarity search with a filter
     results = vector_search.similarity_search_with_score(
         query = query ,
